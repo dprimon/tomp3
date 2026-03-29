df5f138b9248103d434f2bb34e9435c42d6624ad
Daniele Primon <ledebass@gmail.com>
2026-03-29 19:37:28 +0200

    Add concurrency and lint code.

--- flac-to-mp3.sh
+++ flac-to-mp3.sh
@@ -1,28 +1,37 @@
-function flactomp3 {
-  FFMETADATA="$(mktemp)"
-  SAMPLEFILE="$(echo 01*.flac)"
-  ffmpeg -i "$SAMPLEFILE" -f ffmetadata -y "$FFMETADATA"
-  ARTIST="$(sed -e 's/^artist=\(.*\)/\1/Ip' --quiet $FFMETADATA)"
-  ALBUM="$(sed -e 's/^album=\(.*\)/\1/Ip' --quiet $FFMETADATA)"
-  [[ "${ARTIST}${ALBUM}z" == "z" ]] && {
+#!/bin/bash
+
+function tomp3 
+{
+  maxProcesses=16
+  extension="$1"
+  metadataFile="$(mktemp)"
+  sampleFile="$(echo 01*."$extension")"
+  ffmpeg -i "$sampleFile" -f ffmetadata -y "$metadataFile"
+  artist="$(sed -e 's/^artist=\(.*\)/\1/Ip' --quiet "$metadataFile")"
+  album="$(sed -e 's/^album=\(.*\)/\1/Ip' --quiet "$metadataFile")"
+  [[ "${artist}${album}z" == "z" ]] && 
+  {
     echo "Both artist and album are empty. Exiting." 
     return ;
   }
 
-  function escapeFullAlbumName {
-    echo $(printf '%q' "$ARTIST - $ALBUM") 
-  }
-  OUTPUTDIR="$(escapeFullAlbumName)"
+  outputDir="$artist - $album"
 
-  echo Creating "$ARTIST - $ALBUM"
-  mkdir -p "$ARTIST - $ALBUM"
-  for f in *.flac; do 
-    ffmpeg -i "$f" -threads 1 -c:a libmp3lame -b:a 320k -map_metadata 0 "$ARTIST - $ALBUM/${f%.flac}.mp3"; 
-  done
-  cd "$ARTIST - $ALBUM" 
-  rm $FFMETADATA
+  echo Creating "$outputDir"
+  mkdir -p "$outputDir"
+  numProcessesInPool=0;
+  while read -r f;
+  do 
+    ((numProcessesInPool++ < maxProcesses)) || wait -n;
+    ffmpeg -nostdin -i "$f" -threads 1 -c:a libmp3lame -b:a 320k -map_metadata 0 "$outputDir/${f%.$extension}.mp3" &
+  done < <(find . -name "*.$extension" | grep '/[[:digit:]][[:digit:]]')
+  wait
+  rm "$metadataFile"
+  cd "$outputDir" || exit
 }
 
-function split_cue_flac {
+
+function split_cue_flac 
+{
   shnsplit -f "$1" -o flac "$2" -t "%n - %t"
 }
